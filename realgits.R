aeccf7add85e0c9c703015cf4e350971e4d03c5c
Nicholas Pachos <s190535@student.pg.edu.pl>
2024-12-08 12:54:43 +0100

    poprawione kolumny

--- realgits.R
+++ realgits.R
@@ -17,3 +17,19 @@ gg_miss_fct(samochody_new, fct = brand)
 gg_miss_fct(samochody_new, fct = gearbox)
 gg_miss_upset(samochody_new, 
               nsets = 10)
+ggplot(data = samochody_new, aes(x = brand, y = model)) +
+  geom_point() +
+  geom_miss_point() +
+  scale_color_manual(values = c("darkorange","cyan4")) +
+  theme_minimal() 
+
+#data wwrangling
+samochody_new %>%
+  count(brand)
+
+samochody_new %>%
+  count(year)
+colnames(samochody_new)[4] <- "fuel_type"
+view(samochody_new)
+colnames(samochody_new)[4] <- "mileage"
+view(samochody_new)
