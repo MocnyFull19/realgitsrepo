1f1ca9e39f98dd0234947df8d68444d19b117e08
Nicholas Pachos <s190535@student.pg.edu.pl>
2024-12-08 11:51:59 +0100

    .

--- realgits.R
+++ realgits.R
@@ -1,4 +1,19 @@
 #Hejka
 #CZESC
 # eciceicneincei
-pip install -r requirements.txt
+View(samochody_new)
+install.packages(c("tidyverse", "naniar"))
+library(tidyverse)
+library(naniar)
+n_miss(samochody_new)
+prop_miss(samochody_new)
+miss_var_summary(samochody_new)%>% 
+  group_by(samochody_new$brand) %>% 
+  miss_var_summary()
+samochody_new %>% 
+  miss_case_table()
+vis_miss(samochody_new)
+gg_miss_fct(samochody_new, fct = brand)
+gg_miss_fct(samochody_new, fct = gearbox)
+gg_miss_upset(samochody_new, 
+              nsets = 10)
