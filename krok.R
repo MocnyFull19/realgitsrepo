b1c5c77165652bbd57de8a7e3c9565f380edce41
Nicholas Pachos <s190535@student.pg.edu.pl>
2025-01-13 11:44:57 +0100

    poprawione 13.01

--- krok.R
+++ krok.R
@@ -68,14 +68,6 @@ samochody_01$fuel_type <- ifelse(
   samochody_01$fuel_type
 )
 table(is.na(samochody_01$fuel_type))
-# wykorzystanie polyreg
-imputed_data <- mice(samochody_01, method = "polyreg", m = 5, maxit = 10, seed = 123)
-samochody_01_imputed <- complete(imputed_data, 1)
-samochody_01$fuel_type <- samochody_01_imputed$fuel_type
-vis_miss(samochody_01, warn_large_data = FALSE)
-
-#zastosowanie innej default method  logistic regression imputation (binary data, factor with 2 levels) polyreg
-help(mice)
 ##
 # Wyodrębnienie kolumn fuel_type i brand
 samochody_fuel_brand <- samochody_01[, c("fuel_type", "brand"), drop = FALSE]
